390e523653bf96b6aaf02cb2e19fc48e2d1de212
Nguyễn Hứa Tiên <nguyenhuatien26072004@gmail.com>
2023-01-12 20:04:43 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,67 @@
+let allMusic = [{
+        name: "Anh Mới Chính Là Người Yêu Em",
+        artist: "Nguyễn Hứa Tiên",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Bạn Và Bè",
+        artist: "Nguyễn Hứa Tiên",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Bing Chilling x Trái Tim Ngục Tù",
+        artist: "Nguyễn Hứa Tiên",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Cố Nhân Tình",
+        artist: "Nguyễn Hứa Tiên",
+        img: "2",
+        src: "2"
+    },
+    {
+        name: "Gu Han Sì",
+        artist: "Nguyễn Hứa Tiên",
+        img: "3",
+        src: "3"
+    },
+    {
+        name: "Mày Đang Giấu Cái Gì Đó Homie",
+        artist: "Nguyễn Hứa Tiên",
+        img: "4",
+        src: "4"
+    },
+    {
+        name: "Rằng Em Mãi Ở Bên",
+        artist: "Nguyễn Hứa Tiên",
+        img: "5",
+        src: "5"
+    },
+    {
+        name: "Nắm Tay Nhau",
+        artist: "Nguyễn Hứa Tiên",
+        img: "6",
+        src: "6"
+    },
+    {
+        name: "Sợ Ngày Mai Em Đi Mất",
+        artist: "Nguyễn Hứa Tiên",
+        img: "7",
+        src: "7"
+    },
+    {
+        name: "Trót Trao Duyên",
+        artist: "Nguyễn Hứa Tiên",
+        img: "8",
+        src: "8"
+    },
+    {
+        name: "Ngày Thơ ẤU",
+        artist: "Nguyễn Hứa Tiên",
+        img: "9",
+        src: "9"
+    },
+];
